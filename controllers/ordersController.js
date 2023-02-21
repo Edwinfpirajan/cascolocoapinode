a8d435238fdd57c4cf8989063948589408bf7dfc
LabCarlos <carlos.higuera@labcontrol.co>
2023-02-21 10:31:13 -0500

    toDo filterParameterWS-PS

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 const uri = process.env.uri+'orders';
 const key = '&ws_key='+process.env.key;
 const keyJS = '&output_format=JSON&ws_key='+key;
-
+// ToDO hacer fucionar el filter 
+const filterParameter = "=>[2023-00-00%2024:00:00]"
+// Info XML from Prestashop
 const getOrdersSchema = async (req, res) => {
     try {
         const schema = await axios.get(`${uri}?schema=blank${key}`);
@@ -18,7 +20,7 @@ const getOrders = async (req, res) => {
         const nlimit=5;
         const offset=9;
         const display ='?display=full'
-        const filter ="&filter[date_add]=>[2023-00-00%2024:00:00]"
+        const filter ="&filter[date_add]"+filterParameter;
         const sort= "&sort=[date_add_DESC]&date=1"
         const limit = '&limit='+offset+','+nlimit
         const schema = await axios.get(`${uri}${display}${sort}${limit}${keyJS}`);
@@ -28,6 +30,7 @@ const getOrders = async (req, res) => {
         return res.json(pedidos);
     } catch (error) {
         console.log(error);
+        return res.status(error.response.status).json(error.response.data.errors);
     }
 };
 module.exports = {
